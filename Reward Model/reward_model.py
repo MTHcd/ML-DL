e91a2c34c89133e64cceba2ead66f14e76971e31
MTHcd <mathm2551@gmail.com>
2024-12-09 11:58:20 +0100

    Add files via upload

---
+++ Reward Model/reward_model.py
@@ -0,0 +1,8 @@
+
+class RewardModel:
+    def __init__(self):
+        print("Initializing reward model")
+
+    def compute_reward(self, input_ids):
+        print("Computing reward for:", input_ids)
+    
